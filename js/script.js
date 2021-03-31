0e417f4cd191e3e4178470aa279a29e7184545d1
Karl Fälth <karl.falth@elev.ga.ntig.se>
2021-03-31 17:13:22 +0200

    snabb push startar om datorn

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-const menuButton = document.querySelector(".menuButton")
+// const menuButton = document.querySelector(".menuButton")
+const menuList = document.querySelector(".menuList")
 const menuIcon = document.querySelector(".menuIcon")
+
+// function toggleMenu(){
+//     if (menuButton.style.display == "none") {
+//         menuButton.style.display = "initial";}
+//     if (menuList.style.display == "none") {
+//         menuList.style.display = "initial";}
+
+//     setTimeout(function(){
+//         menuButton.classList.toggle("menuInvisible")
+//         menuList.classList.toggle("menuInvisible")
+//         // menuButton.classList.toggle("menuButton")
+//         // menuIcon.classList.toggle("menuInVisible")
+//         menuButton.addEventListener("transitionend", function displayNone(){
+//             if(menuButton.classList == "menuInvisible"){
+//                 menuButton.style.display = "none"}
+//             if(menuList.classList == "menuList menuInvisible"){
+//                 menuList.style.display = "none"}})
+//     }, 1)
+// }
 function toggleMenu(){
-    if (menuButton.style.display == "none") {
-        menuButton.style.display = "initial";
-    }
-        setTimeout(function(){
-        menuButton.classList.toggle("menuInVisible")
-        menuButton.classList.toggle("menuButton")
-        // menuIcon.classList.toggle("menuInVisible")
-        menuButton.addEventListener("transitionend", function displayNone(){
-            if(menuButton.style.display == "none"){
-            menuButton.style.display = "initial";
-            }else {
-                menuButton.style.display = "none"
-            }
-        })
-    }, 100);
+    menuList.classList.toggle("menuToggle")
+    menuIcon.classList.toggle("toggleMenu")
 }
-[menuButton, menuIcon].forEach(item => {item.addEventListener("click", toggleMenu)})
-
-function headerResizer(){
-
-}
-document.addEventListener
+[menuIcon].forEach(item => {item.addEventListener("click", toggleMenu)})
